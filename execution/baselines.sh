35c0fbb45cecc99b369f71a31f668ebcb8800b5d
SherylHYX <He_YX@outlook.com>
2022-10-17 10:19:32 +0000

    remove old param

--- execution/baselines.sh
+++ execution/baselines.sh
@@ -8,7 +8,7 @@ cd ../src
 
 ../../parallel -j15 --resume-failed --results ../Output/basketballbaselines --joblog ../joblog/basketballbaselines python ./train.py --num_trials 10  --no-cuda --season {1} --dataset {2} --all_methods baselines_shorter -All ::: {1985..2014} ::: basketball finer_basketball
 
-../../parallel -j15 --resume-failed --results ../Output/ERObaselines --joblog ../joblog/ERObaselines python ./train.py --p {1} --eta {2} --no-cuda --ERO_style {3} --dataset ERO --all_methods baselines_shorter -All --ambient 0 ::: 0.05 1 ::: 0 0.1 0.2 0.3 0.4 0.5 0.6 0.7 0.8 0.9  ::: uniform gamma
+../../parallel -j15 --resume-failed --results ../Output/ERObaselines --joblog ../joblog/ERObaselines python ./train.py --p {1} --eta {2} --no-cuda --ERO_style {3} --dataset ERO --all_methods baselines_shorter -All  ::: 0.05 1 ::: 0 0.1 0.2 0.3 0.4 0.5 0.6 0.7 0.8 0.9  ::: uniform gamma
 
 ../../parallel -j15 --resume-failed --results ../Output/realAmvr --joblog ../joblog/realAmvr python ./train.py --num_trials 10 --dataset {1} --all_methods mvr --no-cuda -All ::: faculty_business faculty_cs faculty_history animal
 
@@ -20,5 +20,5 @@ cd ../src
 
 ../../parallel -j15 --resume-failed --results ../Output/basketballmvr --joblog ../joblog/basketballmvr python ./train.py --num_trials 10  --no-cuda --season {1} --dataset {2} --all_methods mvr -All ::: {1985..2014} ::: basketball finer_basketball
 
-../../parallel -j15 --resume-failed --results ../Output/EROmvr --joblog ../joblog/EROmvr python ./train.py --p {1} --eta {2} --no-cuda --ERO_style {3} --dataset ERO --all_methods mvr -All --ambient 0 ::: 0.05 1 ::: 0 0.1 0.2 0.3 0.4 0.5 0.6 0.7 0.8 0.9  ::: uniform gamma
+../../parallel -j15 --resume-failed --results ../Output/EROmvr --joblog ../joblog/EROmvr python ./train.py --p {1} --eta {2} --no-cuda --ERO_style {3} --dataset ERO --all_methods mvr -All  ::: 0.05 1 ::: 0 0.1 0.2 0.3 0.4 0.5 0.6 0.7 0.8 0.9  ::: uniform gamma
 
